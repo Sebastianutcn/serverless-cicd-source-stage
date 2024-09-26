2c1eb71f9b5b5dec40b57e9c9e0d41ec3f03579e
Sebastian Adrian Babiciu <72445123+Sebastianutcn@users.noreply.github.com>
2024-09-26 14:13:11 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+<script>
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    // Send the data to API Gateway (which triggers Lambda)
+    fetch('https://api-id.execute-api.region.amazonaws.com/Prod/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        message: message
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  });
+</script>
